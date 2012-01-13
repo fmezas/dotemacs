8fcd5a293239e1016a30e3f8093e37146c9ca8cd
Francisco Meza <fmezas@gmail.com>
2012-01-13 11:57:06 -0800

    pymacs, rope and pylint working nicely with virtualenv within emacs

--- dotemacs.el
+++ dotemacs.el
@@ -2,7 +2,88 @@
 
 (setq inhibit-splash-screen t)
 (iswitchb-mode)
+(setq-default indent-tabs-mode nil)
 (menu-bar-mode 0)
+(tool-bar-mode 0)
+
+(setq org-agenda-files '("~/org"))
+(setq custom-theme-directory "~/.emacs.d/themes")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;PYTHON STUFF;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; python-mode.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/python-mode.el-6.0.4")
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+;;;;;;;;;;;;;;;;;;
+;; virtualenv
+;;;;;;;;;;;;;;;;;;
+(defun add-to-PATH (dir)
+  "Add the specified path element to the Emacs PATH"
+  (interactive "DEnter directory to be added to PATH: ")
+  (if (file-directory-p dir)
+      (setenv "PATH"
+              (concat (expand-file-name dir)
+                      path-separator
+                      (getenv "PATH")))))
+(defun activate-virtualenv (dir)
+  (setenv "VIRTUAL_ENV" dir)
+  (add-to-PATH (concat dir "/bin"))
+  (add-to-list 'exec-path (concat dir "/bin")))
+(activate-virtualenv "~/.virtualenvs/cherrypylab")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; pymacs and ropemacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; pymacs
+  (add-to-list 'load-path "~/.emacs.d/pymacs")
+  (autoload 'pymacs-apply "pymacs")
+  (autoload 'pymacs-call "pymacs")
+  (autoload 'pymacs-eval "pymacs" nil t)
+  (autoload 'pymacs-exec "pymacs" nil t)
+  (autoload 'pymacs-load "pymacs" nil t)
+
+  ;;(eval-after-load "pymacs"
+  ;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+
+  ;; ropemacs
+  (setq ropemacs-enable-shortcuts t)
+  (pymacs-load "ropemacs" "rope-")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; end of pymacs and ropemacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; flymake-cursor
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/flymake-cursor")
+(require 'flymake-cursor)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; enable flymake automatically
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; flymake pylint integration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "epylint"  (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;END OF PYTHON STUFF;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;
 ;; yasnippet
@@ -10,7 +91,6 @@
 (add-to-list 'load-path "~/.emacs.d/yasnippet")
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/global-mode 1)
-
 ;;;;;;;;;;;;;;;;;;
 ;; auto-complete
 ;;;;;;;;;;;;;;;;;;
@@ -40,84 +120,25 @@
 ;;                     (list #'autopair-default-handle-action
 ;;                           #'autopair-python-triple-quote-action))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; flymake-cursor
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/flymake-cursor")
-(require 'flymake-cursor)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable flymake automatically
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;PYTHON STUFF;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; python-mode.el
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (add-to-list 'load-path "~/.emacs.d/python-mode.el-6.0.4")
-;; (autoload 'python-mode "python-mode" "Python Mode." t)
-;; (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-;; (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; flymake pyflakes integration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(when (load "flymake" t) 
-  (defun flymake-pyflakes-init () 
-     ; Make sure it's not a remote buffer or flymake would not work
-     ;;(when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
-      (let* ((temp-file (flymake-init-create-temp-buffer-copy 
-                         'flymake-create-temp-inplace)) 
-             (local-file (file-relative-name 
-                          temp-file 
-                          (file-name-directory buffer-file-name)))) 
-        (list "pyflakes" (list local-file))))
-  ;;)
-  (add-to-list 'flymake-allowed-file-name-masks 
-               '("\\.py\\'" flymake-pyflakes-init))) 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; pymacs
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/pymacs")
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-;;(eval-after-load "pymacs"
-;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ropemacs
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq ropemacs-enable-shortcuts t)
-(pymacs-load "ropemacs" "rope-")
-
-;;;;;;;;;;;;;;;;;;
-;; virtualenv
-;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/virtualenv.el")
-(require 'virtualenv)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;END OF PYTHON STUFF;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector (vector "#4d4d4c" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#ffffff"))
+ '(custom-safe-themes (quote ("69546801bd9c98eeb7246a3d39497abeced1d11e" "a677b91598bc31bc159847fa2a3c0f525c7bf5bb" "d818d364712b551c535b952b3aa089c5941ef284" "39327baac0e924fc06c561986ed6fff862df8e1d" "3c221cf1a0a4172917772c71da5c4d5e1d4f98c4" default)))
  '(ns-alternate-modifier (quote control))
  '(ns-command-modifier (quote meta)))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
+
+(add-to-list 'load-path "~/.emacs.d/package")
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(put 'downcase-region 'disabled nil)
