325a295075fe8724650b7e1de7983b721fb62be3
Francisco Meza <francisco.meza@invitae.com>
2016-10-10 16:39:25 -0700

    newer version. non incremental changes

--- dotemacs.el
+++ dotemacs.el
@@ -1,144 +1,82 @@
-;;https://github.com/fmezas/dotemacs
+(defun my-turn-modes (param &rest modes)
+  ;;; Applies the parameter to the specified modes
+  (mapcar #'(lambda (mode)
+              (funcall mode param)) modes))
 
-(setq inhibit-splash-screen t)
-(iswitchb-mode)
-(setq-default indent-tabs-mode nil)
-(menu-bar-mode 0)
-(tool-bar-mode 0)
+;; package management
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+(package-initialize)
 
-(setq org-agenda-files '("~/org"))
-(setq custom-theme-directory "~/.emacs.d/themes")
+;; autocomplete for emacs lisp mode
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;PYTHON STUFF;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; some defaults
+(my-turn-modes 1
+ 'global-hl-line-mode
+ ;; 'which-key-mode
+ 'whole-line-or-region-mode
+ 'ido-mode
+ 'paredit-mode
+ 'show-paren-mode
+ 'rainbow-delimiters-mode
+ 'column-number-mode
+ 'global-auto-complete-mode)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; python-mode.el
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/python-mode.el-6.0.4")
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-;;;;;;;;;;;;;;;;;;
-;; virtualenv
-;;;;;;;;;;;;;;;;;;
-(defun add-to-PATH (dir)
-  "Add the specified path element to the Emacs PATH"
-  (interactive "DEnter directory to be added to PATH: ")
-  (if (file-directory-p dir)
-      (setenv "PATH"
-              (concat (expand-file-name dir)
-                      path-separator
-                      (getenv "PATH")))))
-(defun activate-virtualenv (dir)
-  (setenv "VIRTUAL_ENV" dir)
-  (add-to-PATH (concat dir "/bin"))
-  (add-to-list 'exec-path (concat dir "/bin")))
-(activate-virtualenv "~/.virtualenvs/cherrypylab")
+(setq-default
+ which-key-idle-delay 0.2)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; pymacs and ropemacs
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  ;; pymacs
-  (add-to-list 'load-path "~/.emacs.d/pymacs")
-  (autoload 'pymacs-apply "pymacs")
-  (autoload 'pymacs-call "pymacs")
-  (autoload 'pymacs-eval "pymacs" nil t)
-  (autoload 'pymacs-exec "pymacs" nil t)
-  (autoload 'pymacs-load "pymacs" nil t)
+(my-turn-modes 0
+ 'scroll-bar-mode
+ 'tool-bar-mode
+ 'menu-bar-mode)
 
-  ;;(eval-after-load "pymacs"
-  ;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+(setq-default
+ inhibit-startup-screen t
+ indent-tabs-mode nil)
 
-  ;; ropemacs
-  (setq ropemacs-enable-shortcuts t)
-  (pymacs-load "ropemacs" "rope-")
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; end of pymacs and ropemacs
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-flycheck-mode)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; flymake-cursor
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/flymake-cursor")
-(require 'flymake-cursor)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable flymake automatically
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; flymake pylint integration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(when (load "flymake" t)
-  (defun flymake-pylint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "epylint"  (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pylint-init)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;END OF PYTHON STUFF;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;
-;; yasnippet
-;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/yasnippet")
-(require 'yasnippet) ;; not yasnippet-bundle
-(yas/global-mode 1)
-;;;;;;;;;;;;;;;;;;
-;; auto-complete
-;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/auto-complete-patched")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete-patched//ac-dict")
-(ac-config-default)
-(setq ac-auto-start nil)
-(ac-set-trigger-key "TAB")
-
-;;;;;;;;;;;;;;;;;;;;;
-;; autopair
-;;;;;;;;;;;;;;;;;;;;;
-;; commented out because it breaks auto-complete's use of <ret> key to
-;; insert completions (yasnippet)
-;; (add-to-list 'load-path "~/.emacs.d/autopair")
-;; (autoload 'autopair-global-mode "autopair" nil t)
-;; (autopair-global-mode)
-;; (add-hook 'lisp-mode-hook
-;;           #'(lambda () (setq autopair-dont-activate t)))
-;; ;; help autopair with python single and triple quotes
-;; (add-hook 'python-mode-hook
-;;           #'(lambda ()
-;;               (push '(?' . ?')
-;;                     (getf autopair-extra-pairs :code))
-;;               (setq autopair-handle-action-fns
-;;                     (list #'autopair-default-handle-action
-;;                           #'autopair-python-triple-quote-action))))
-
+(exec-path-from-shell-initialize)
+(add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+   '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup)))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector (vector "#4d4d4c" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#ffffff"))
- '(custom-safe-themes (quote ("69546801bd9c98eeb7246a3d39497abeced1d11e" "a677b91598bc31bc159847fa2a3c0f525c7bf5bb" "d818d364712b551c535b952b3aa089c5941ef284" "39327baac0e924fc06c561986ed6fff862df8e1d" "3c221cf1a0a4172917772c71da5c4d5e1d4f98c4" default)))
- '(ns-alternate-modifier (quote control))
- '(ns-command-modifier (quote meta)))
+ '(js-indent-level 2)
+ '(js2-basic-offset 2)
+ '(json-reformat:indent-width 2)
+ '(markdown-command "/opt/local/bin/multimarkdown"))
+
+
+
+;; key bindings for OSX
+(when (eq system-type 'darwin) ;; mac specific settings
+  (setq mac-option-modifier 'control)
+  (setq mac-command-modifier 'meta)
+  (global-set-key [kp-delete] 'delete-char)) ;; sets fn-delete to be right-delete
+
+(global-set-key (kbd "C-x O") 'previous-multiframe-window)
+
+(load-theme 'zenburn t)
+
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(add-to-list 'load-path "~/.emacs.d/package")
-(require 'package)
-(package-initialize)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
